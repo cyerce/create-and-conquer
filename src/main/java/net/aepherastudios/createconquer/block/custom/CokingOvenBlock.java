290014d1610773fc510472ff03ae151d97031607
MENTIY <164089516+MENTIY@users.noreply.github.com>
2024-12-06 22:13:40 -0700

    coking oven eddits (Mentiy was Here =>)
    Signed-off-by: MENTIY <164089516+MENTIY@users.noreply.github.com>

--- src/main/java/net/aepherastudios/createconquer/block/custom/CokingOvenBlock.java
+++ src/main/java/net/aepherastudios/createconquer/block/custom/CokingOvenBlock.java
@@ -1,12 +1,90 @@
 package net.aepherastudios.createconquer.block.custom;
 
+import net.aepherastudios.createconquer.block.entity.ArcFurnaceBlockEntity;
+import net.aepherastudios.createconquer.block.entity.CCBlockEntities;
+import net.aepherastudios.createconquer.block.entity.CokingOvenBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.crafting.SmeltingRecipe;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FurnaceBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.network.NetworkHooks;
 
 import javax.annotation.Nullable;
 
-public class CokingOvenBlock extends FurnaceBlock {
+public class CokingOvenBlock extends BaseEntityBlock {
+    public static final VoxelShape SHAPE = Block.box(0,0,0,16,16,16);
+
     public CokingOvenBlock(Properties pProperties) {
+
         super(pProperties);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext){
+        return SHAPE;
+    }
+
+    /* Block Entity Stuff? */
+
+    @Override
+    public RenderShape getRenderShape(BlockState pState) {
+        return RenderShape.MODEL;
+    }
+
+    @Override
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if(pState.getBlock() != pNewState.getBlock()){
+            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+            if(blockEntity instanceof CokingOvenBlockEntity){
+                ((CokingOvenBlockEntity) blockEntity).drops();
+            }
+        }
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if (!pLevel.isClientSide()) {
+            BlockEntity entity = pLevel.getBlockEntity(pPos);
+            if(entity instanceof ArcFurnaceBlockEntity) {
+                NetworkHooks.openScreen(((ServerPlayer)pPlayer), (CokingOvenBlockEntity)entity, pPos);
+            } else {
+                throw new IllegalStateException("WHO IN THE WORLD TOOK OUR CONTAINER PROVIDER!!");
+            }
+        }
+
+        return InteractionResult.sidedSuccess(pLevel.isClientSide());
+    }
+
+    @Override
+    public @org.jetbrains.annotations.Nullable BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+
+        return new ArcFurnaceBlockEntity(blockPos, blockState);
+    }
+
+    @javax.annotation.Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+        if(pLevel.isClientSide()) {
+            return null;
+        }
+
+        return createTickerHelper(pBlockEntityType, CCBlockEntities.COKING_OVEN_BE.get(),
+                (pLevel1, pPos, pState1, pBlockEntity) -> pBlockEntity.tick(pLevel1, pPos, pState1));
     }
 }
--- src/main/java/net/aepherastudios/createconquer/block/entity/CokingOvenBlockEntity.java
+++ src/main/java/net/aepherastudios/createconquer/block/entity/CokingOvenBlockEntity.java
@@ -1,13 +1,145 @@
 package net.aepherastudios.createconquer.block.entity;
 
-import net.aepherastudios.createconquer.recipe.CCRecipes;
+import net.aepherastudios.createconquer.block.CCBlocks;
+import net.aepherastudios.createconquer.item.CCItems;
+import net.aepherastudios.createconquer.screen.ArcFurnaceMenu;
+import net.aepherastudios.createconquer.screen.CokingOvenMenu;
+import net.aepherastudios.createconquer.screen.NuclearReactorMenu;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.entity.FurnaceBlockEntity;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.Containers;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class CokingOvenBlockEntity extends FurnaceBlockEntity {
+public class CokingOvenBlockEntity extends BlockEntity implements MenuProvider {
+    private final ItemStackHandler itemHandler = new ItemStackHandler(2){
+        @Override
+        protected void onContentsChanged(int slot){
+            setChanged();
+        }
+
+        @Override
+        public boolean isItemValid(int slot, ItemStack stack){
+            return switch(slot) {
+                case 0 -> stack.getItem() == CCItems.LEAD_POWDER.get();
+                default -> super.isItemValid(slot, stack);
+            };
+        }
+    };
+
+    public static final int SLOT1 = 0;
+    public static final int SLOT2 = 1;
+
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+
+    protected ContainerData data;
+    private int progress;
+    private int maxProgress = 78;
+
 
     public CokingOvenBlockEntity(BlockPos pPos, BlockState pBlockState) {
-        super(pPos, pBlockState);
+        super(CCBlockEntities.COKING_OVEN_BE.get(), pPos, pBlockState);
+        this.data = new ContainerData() {
+            @Override
+            public int get(int i) {
+                return switch (i){
+                    case 0 -> CokingOvenBlockEntity.this.progress;
+                    case 1 -> CokingOvenBlockEntity.this.maxProgress;
+                    default -> 0;
+                };
+            }
+
+            @Override
+            public void set(int i, int i1) {
+                switch (i){
+                    case 0 -> CokingOvenBlockEntity.this.progress = i1;
+                    case 1 -> CokingOvenBlockEntity.this.maxProgress = i1;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        };
     }
+
+    public void drops() {
+        SimpleContainer invintory = new SimpleContainer(itemHandler.getSlots());
+        for(int i = 0; i < itemHandler.getSlots(); i++){
+            invintory.setItem(i, itemHandler.getStackInSlot(i));
+        }
+
+        Containers.dropContents(this.level, this.worldPosition, invintory);
+
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.literal("Coking Oven");
+    }
+
+    @Override
+    public @Nullable AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
+        return new CokingOvenMenu(pContainerId, pPlayerInventory, this, this.data);
+    }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
+        if(cap == ForgeCapabilities.ITEM_HANDLER){
+            return lazyItemHandler.cast();
+        }
+
+        return super.getCapability(cap, side);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        lazyItemHandler.invalidate();
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag pTag) {
+        pTag.put("inventory", itemHandler.serializeNBT());
+
+        super.saveAdditional(pTag);
+    }
+
+    @Override
+    public void load(CompoundTag pTag) {
+        super.load(pTag);
+
+        itemHandler.deserializeNBT(pTag.getCompound("inventory"));
+    }
+
+    public void tick(Level level, BlockPos pPos, BlockState pState){
+
+    }
+
+
 }
